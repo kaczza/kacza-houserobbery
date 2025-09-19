f9968f3c3df1c16311afed45b0584592196ea8a4
Kacza <107073354+kaczza@users.noreply.github.com>
2025-09-19 17:46:31 +0200

    Updated server.lua
    Optimized it

--- server/server.lua
+++ server/server.lua
@@ -1,66 +1,73 @@
-ESX = nil
 ESX = exports['es_extended']:getSharedObject()
-
+local Lastrob = 0
+-- check item
 RegisterServerEvent('kz_houserobery:checkitem')
 AddEventHandler('kz_houserobery:checkitem', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local item = exports.ox_inventory:GetItem(source, Config.ItemRequired, nil, 1)
-    if Config.RequiredItem then
-        if xPlayer.getInventoryItem(Config.ItemRequired).count >= 1 then 
-            xPlayer.triggerEvent('kz_houserobery:startminigame')
-        else
-            xPlayer.showNotification(Config.Text.nolockpick)
-        end
-    else
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+
+    if not Config.RequiredItem then
         xPlayer.triggerEvent('kz_houserobery:startminigame')
-        print('mono')
+        return
+    end
+
+    local itemCount = xPlayer.getInventoryItem(Config.ItemRequired).count
+    if itemCount and itemCount > 0 then
+        xPlayer.triggerEvent('kz_houserobery:startminigame')
+    else
+        xPlayer.showNotification(Config.Text.nolockpick)
     end
 end)
 
-
+-- give loot
 RegisterServerEvent('kz_houserobery:giveloot')
-AddEventHandler('kz_houserobery:giveloot', function ()
+AddEventHandler('kz_houserobery:giveloot', function()
     local xPlayer = ESX.GetPlayerFromId(source)
-    local random = math.random(1,3) 
-    if random == 1 then
-        xPlayer.addInventoryItem(Config.Loot.badloot[math.random(#Config.Loot.badloot)], math.random(Config.Cant.badloot[1], Config.Cant.badloot[2]))
-    elseif random == 2 then
-        xPlayer.addInventoryItem(Config.Loot.mediumloot[math.random(#Config.Loot.mediumloot)], math.random(Config.Cant.mediumloot[1], Config.Cant.mediumloot[2]))
-    elseif random == 3 then
-        xPlayer.addInventoryItem(Config.Loot.goodloot[math.random(#Config.Loot.goodloot)], tonumber(math.random(Config.Cant.goodloot[1], Config.Cant.goodloot[2])))
+    if not xPlayer then return end
+
+    local roll = math.random(1, 3)
+    local lootTable, countRange
+
+    if roll == 1 then
+        lootTable, countRange = Config.Loot.badloot, Config.Cant.badloot
+    elseif roll == 2 then
+        lootTable, countRange = Config.Loot.mediumloot, Config.Cant.mediumloot
+    else
+        lootTable, countRange = Config.Loot.goodloot, Config.Cant.goodloot
     end
+
+    local item = lootTable[math.random(#lootTable)]
+    local amount = math.random(countRange[1], countRange[2])
+    xPlayer.addInventoryItem(item, amount)
 end)
 
-
-ESX.RegisterServerCallback('kz_houserobery:countpolice', function(source, cb)
+-- count cops
+ESX.RegisterServerCallback('kz_houserobery:countpolice', function(_, cb)
     local police = 0
-    local xPlayers = ESX.GetPlayers()
-    for i = 1, #xPlayers do
-        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-        if xPlayer.job.name == Config.JobName then
+    for _, playerId in ipairs(ESX.GetPlayers()) do
+        local xPlayer = ESX.GetPlayerFromId(playerId)
+        if xPlayer and xPlayer.job and xPlayer.job.name == Config.JobName then
             police = police + 1
         end
     end
-    if police >= Config.CopsRequired then
-        cb(true)
-    else
+    cb(police >= Config.CopsRequired)
+end)
+
+-- cooldown
+ESX.RegisterServerCallback('kz_houserobery:house:cooldown', function(source, cb)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if not xPlayer then return cb(false) end
+
+    if Lastrob ~= 0 and (os.time() - Lastrob) < Config.Cooldown then
+        xPlayer.showNotification(Config.Text.wait)
         cb(false)
+    else
+        cb(true)
     end
 end)
 
-ESX.RegisterServerCallback('kz_houserobery:house:cooldown', function(source, cb)
-    local xPlayer = ESX.GetPlayerFromId(source)
-	if (os.time() - Config.Lastrob) < Config.Cooldown and Config.Lastrob ~= 0 then
-		cb(false)
-        xPlayer.showNotification(Config.Text.wait)
-	else
-		cb(true)
-	end
-end)
-
+-- set last rob time
 RegisterServerEvent('kz_houserobery:time')
 AddEventHandler('kz_houserobery:time', function()
-    Config.Lastrob = os.time()
+    Lastrob = os.time()
 end)
-
-
